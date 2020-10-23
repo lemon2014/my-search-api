bf410faffdd91b756073e68c879ed98cdfc012ce
longmeng <longmeng0411@163.com>
2020-10-23 12:10:00 +0800

    init

---
+++ src/main/java/cn/com/search/Application.java
@@ -0,0 +1,15 @@
+package cn.com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+@ComponentScan(basePackages = {"cn.com.search"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
